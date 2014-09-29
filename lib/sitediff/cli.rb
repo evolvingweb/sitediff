8732ccfb605d268abfcaaa84ed8e2a8219308724
Dave Vasilevsky <dave@vasilevsky.ca>
2014-09-29 20:24:26 +0000

    Slightly more sane help output

--- lib/sitediff/cli.rb
+++ lib/sitediff/cli.rb
@@ -14,30 +14,30 @@ class SiteDiff
     option 'dump-dir',
       :type => :string,
       :default => "./output/",
-      :banner => "Location to write the output to."
+      :desc => "Location to write the output to."
     option 'paths-from-file',
       :type => :string,
-      :banner => "File listing URL paths to run on against <before> and <after> sites."
+      :desc => "File listing URL paths to run on against <before> and <after> sites."
     option 'paths-from-failures',
       :type => :boolean,
       :default => FALSE,
-      :banner => "Equivalent to --paths-from-file=<DUMPDIR>/failures.txt"
+      :desc => "Equivalent to --paths-from-file=<DUMPDIR>/failures.txt"
     option 'before-url',
       :required => true,
       :type => :string,
-      :banner => "URL used to fetch the before HTML. Acts as a prefix to specified paths"
+      :desc => "URL used to fetch the before HTML. Acts as a prefix to specified paths"
     option 'after-url',
       :required => true,
       :type => :string,
-      :banner => "URL used to fetch the after HTML. Acts as a prefix to specified paths."
+      :desc => "URL used to fetch the after HTML. Acts as a prefix to specified paths."
     option 'before-url-report',
       :type => :string,
-      :banner => "Before URL to use for reporting purposes. Useful if port forwarding."
+      :desc => "Before URL to use for reporting purposes. Useful if port forwarding."
     option 'after-url-report',
       :type => :string,
-      :banner => "After URL to use for reporting purposes. Useful if port forwarding."
+      :desc => "After URL to use for reporting purposes. Useful if port forwarding."
     #FIXME description is not correct
-    desc "diff [OPTIONS] <BEFORE> <AFTER> [CONFIGFILES]", "Perform systematic diff on given URLs"
+    desc "diff [OPTIONS] [CONFIGFILES]", "Perform systematic diff on given URLs"
     def diff(*config_files)
       sitediff = SiteDiff.new(config_files)
       sitediff.before = options['before-url']
