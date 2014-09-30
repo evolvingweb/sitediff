e172640c9d3a6094978e39c1fd6acae2d6d4bb91
Amir Kadivar <amir@amirkdv.ca>
2014-09-30 18:43:49 -0400

    add simple test as example

--- spec/unit/sanitize_spec.rb
+++ spec/unit/sanitize_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
+require 'nokogiri'
 
 describe SiteDiff::Util::Sanitize do
-  describe '::perform_dom_transform' do
-    it 'works' do
-      expect(false).to be_false
+  describe '::remove_spacing' do
+    it 'normalizes space but only within text nodes' do
+      doc = Nokogiri::HTML('<html><body  class="x  y">  z  </body></html>')
+      SiteDiff::Util::Sanitize::remove_spacing(doc)
+      expect(doc.to_s).to include '<html><body class="x  y"> z </body></html>'
     end
   end
 end
-
