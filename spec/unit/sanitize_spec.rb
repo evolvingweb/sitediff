d9777f3f0f9f6b549e9446a55ff9b5332ee40462
Dave Vasilevsky <dave@vasilevsky.ca>
2014-10-03 21:48:40 +0000

    Add specs

--- spec/unit/sanitize_spec.rb
+++ spec/unit/sanitize_spec.rb
@@ -16,5 +16,29 @@ describe SiteDiff::Util::Sanitize do
       output = SiteDiff::Util::Sanitize.sanitize(input, {})
       expect(output).to include "\u2014"
     end
+
+    it "can perform a simple regex rule" do
+      input = '<p>test something</p>'
+      config = { 'sanitization' => [ { 'pattern' => 'something' } ] }
+      expect(SiteDiff::Util::Sanitize.sanitize(input, config)).to include(
+        '<p>test </p>')
+    end
+    it "can perform a more complex regex " do
+      input = '<input type="hidden" name="form_build_id" value="form-1cac6b5b6141a72b2382928249605fb1"/>'
+      config = { 'sanitization' => [ {
+        'pattern' => '<input type="hidden" name="form_build_id" value="form-[a-zA-Z0-9_-]+" *\/?>',
+        'selector' => 'input',
+        'substitute' => '<input type="hidden" name="form_build_id" value="__form_build_id__">'
+      } ] }
+      expect(SiteDiff::Util::Sanitize.sanitize(input, config)).to include(
+        '<input type="hidden" name="form_build_id" value="__form_build_id__"/>')
+    end
   end
+
+  # TODO:
+  # regex captures, regex selectors
+  # selector
+  # transforms (unwrap, unwrap_root, remove_class, remove)
+  # overall checks
+  # spec out whether 'body' should be added? Frag vs Doc
 end
