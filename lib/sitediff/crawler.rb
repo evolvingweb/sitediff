94091e584bf96bd07de5edaec6e3f93d28142d14
Dave Vasilevsky <dave@vasilevsky.ca>
2015-04-30 12:08:00 -0400

    Fix cache key issue

--- lib/sitediff/crawler.rb
+++ lib/sitediff/crawler.rb
@@ -22,6 +22,9 @@ class Crawler
 
   # Handle a newly found relative URI
   def add_uri(rel, depth)
+    # Force encoding to UTF-8, for sane cache keys
+    rel.encode!('UTF-8')
+
     return if @found.include? rel
     @found << rel
 
