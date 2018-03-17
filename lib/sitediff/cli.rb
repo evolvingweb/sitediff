1416a926269f24187bb77684d16281c11d3623ef
Muhammad Inam Ur Rasheed <mohdinamshah@gmail.com>
2018-03-17 18:49:39 +0530

    Catch all exceptions and provide useful message & exit code.

--- lib/sitediff/cli.rb
+++ lib/sitediff/cli.rb
@@ -105,6 +105,9 @@ class SiteDiff
       SiteDiff.log "Invalid configuration: #{e.message}", :error
     rescue SiteDiffException => e
       SiteDiff.log e.message, :error
+    rescue Exception => e
+      SiteDiff.log e.message, :error
+      exit(3)
     else # no exception was raised
       # Thor::Error  --> exit(1), guaranteed by exit_on_failure?
       # Failing diff --> exit(2), populated above
